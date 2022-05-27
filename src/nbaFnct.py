57f56945baabf8a3ef7f06900d0244127f57da1c
delare <emmalito.delar@grenoble-inp.org>
2022-05-27 09:27:17 +0200

    Factorisation du NBA

---
+++ src/nbaFnct.py
@@ -0,0 +1,15 @@
+"""
+    Source code of the different functions that
+    we use in the notebook 4_NextBestAction
+"""
+
+# Classical libraries
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+
+def test():
+    return 1
