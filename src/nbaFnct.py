2681221185781ccd135ea6d5bef9c20d2a251fac
delare <emmalito.delar@grenoble-inp.org>
2022-06-07 09:25:48 +0200

    Data visualisation

--- src/nbaFnct.py
+++ src/nbaFnct.py
@@ -10,6 +10,34 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-
-def test():
-    return 1
+def NBA(Rc, productType):
+    index=0
+    D_lessrisky=[]
+    P_lessrisky=[]
+    D_morerisky=[]
+    P_morerisky=[]
+    for index in range(len(productType)):
+        if Rc-productType["Risk"].values[index] >= 0:
+            D_lessrisky.append((Rc-productType["Risk"].values[index]))
+            P_lessrisky.append(productType.index.values[index])
+        if Rc-productType["Risk"].values[index] < 0:
+            D_morerisky.append((productType["Risk"].values[index]-Rc))
+            P_morerisky.append(productType.index.values[index])
+            
+    if len(P_morerisky)>0 and len(P_lessrisky)>1:
+        if D_lessrisky[1] < D_morerisky[-1]:
+            D= [[1-D_lessrisky[0]/(D_lessrisky[0]+D_lessrisky[1]), 1-D_lessrisky[1]/(D_lessrisky[0]+D_lessrisky[1])],[0,0]]
+            P= [[P_lessrisky[0], P_lessrisky[1]],[0,0]]
+        else:
+            D= [[(1-D_lessrisky[0]/(D_lessrisky[0]+D_morerisky[-1])),0],[(1-D_morerisky[-1]/(D_lessrisky[0]+D_morerisky[-1])),0]]
+            P= [[P_lessrisky[0],0], [P_morerisky[-1],0]]
+    elif len(P_lessrisky)==1 :
+            D= [[1-D_lessrisky[0]/(D_lessrisky[0]+D_morerisky[-1]),0],[1-D_morerisky[-1]/(D_lessrisky[0]+D_morerisky[-1]),0]]
+            P= [[P_lessrisky[0],0], [P_morerisky[-1],0]]
+    elif len(P_lessrisky)==0 :
+            D= [[0,0],[1-D_morerisky[-1]/(D_morerisky[-1]+D_morerisky[-2]), 1-D_morerisky[-2]/(D_morerisky[-1]+D_morerisky[-2])]]
+            P= [[0,0],[P_morerisky[-1], P_morerisky[-2]]]
+    elif len(P_morerisky)==0 :
+            D= [[1-D_lessrisky[0]/(D_lessrisky[0]+D_lessrisky[1]), 1-D_lessrisky[1]/(D_lessrisky[0]+D_lessrisky[1])],[0,0]]
+            P= [[P_lessrisky[0], P_lessrisky[1]],[0,0]]   
+    return([D,P])
